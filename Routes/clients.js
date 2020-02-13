cd7c5155e083408a6316281425fdeef2749aea80
Adarsh077 <adarshco077@gmail.com>
2020-02-13 21:05:10 +0530

    changed PUT /client route params

--- Routes/clients.js
+++ Routes/clients.js
@@ -1,42 +1,42 @@
-const router = require('express').Router()
-	, clients = require('../Models/Clients');
+const router = require("express").Router(),
+  clients = require("../Models/Clients");
 
-router.get('/:id', (req, res, next) => {
-	clients
-		.findById(req.params.id)
-		.then(client => res.send(client))
-		.catch(next);
+router.get("/:id", (req, res, next) => {
+  clients
+    .findById(req.params.id)
+    .then(client => res.send(client))
+    .catch(next);
 });
 
-router.get('/', (req, res, next) => {
-	clients
-		.find()
-		.then(clients => res.send(clients))
-		.catch(next);
+router.get("/", (req, res, next) => {
+  clients
+    .find()
+    .then(clients => res.send(clients))
+    .catch(next);
 });
 
-router.post('/', (req, res, next) => {
-	clients
-		.create(req.body)
-		.then(client => {
-			console.log(client)
-			res.send(client)
-		})
-		.catch(next);
+router.post("/", (req, res, next) => {
+  clients
+    .create(req.body)
+    .then(client => {
+      console.log(client);
+      res.send(client);
+    })
+    .catch(next);
 });
 
-router.put('/:id', (req, res, next) => {
-	clients
-		.findByIdAndUpdate(req.params.id, req.body, { new: true })
-		.then(client => res.send(client))
-		.catch(next);
+router.put("/", (req, res, next) => {
+  clients
+    .findByIdAndUpdate(req.body._id, req.body, { new: true })
+    .then(client => res.send(client))
+    .catch(next);
 });
 
-router.delete('/:id', (req, res, next) => {
-	clients
-		.findByIdAndDelete(req.params.id)
-		.then(client => res.send(client))
-		.catch(next);
-})
+router.delete("/:id", (req, res, next) => {
+  clients
+    .findByIdAndDelete(req.params.id)
+    .then(client => res.send(client))
+    .catch(next);
+});
 
 module.exports = router;
