6c733faac6bd65f455253f5af28c05d62e8cf8cd
Adarsh077 <adarshco077@gmail.com>
2020-02-21 10:09:05 +0530

    changed pass

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 mongoose
-	.connect('mongodb+srv://jhon:Lvc0wFz6L6gh24eJ@movies-uw6xa.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-	.then(() => console.log("Connected to MongoDB"))
-	.catch(err => console.log(err));
+  .connect(
+    "mongodb+srv://jhon:Lvc0wFz6L6gh24eJ@movies-uw6xa.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/client', require('./Routes/clients'));
+app.use("/client", require("./Routes/clients"));
 
-app.post('/authenticate', (req, res) => res.send(req.body.password === "piyushele"));
+app.post("/authenticate", (req, res) =>
+  res.send(req.body.password === "kamlesh@07")
+);
 
 app.use((err, req, res, next) => res.send(err));
 
